ba472c8061b875fa931462757f0e0ce0d608b3b4
Sean M Gallagher <sean.gallagher@arstechnica.com>
2016-11-17 12:58:42 -0500

    Add files via upload

---
+++ autobearduino.ino
@@ -0,0 +1,45 @@
+
+#include <Servo.h>
+
+Servo myservo;
+int val = 0; 
+char serialData;
+boolean animate = false;
+void setup()
+{
+  myservo.attach(9);
+  myservo.write(90);
+  Serial.begin(9600);
+  while (!Serial) {;} // wait for serial port to connect. Needed for Leonardo only
+}
+
+void serialEvent()
+{
+  while(Serial.available())
+  {
+    serialData = (char)Serial.read();
+    if (serialData == 'g'){
+      animate = true;
+      }
+    if (serialData == 'x'){
+      animate = false;
+    }
+  }  
+}    
+    
+void loop()
+{
+  if (animate == true){
+   for (val =120; val >= 1; val -= 10)
+   {
+    myservo.write(val);
+    delay(15);
+    }
+   for (val = 1; val <= 120; val += 10)
+   {
+     myservo.write(val);
+    delay(15);
+   }
+       
+  }
+}
